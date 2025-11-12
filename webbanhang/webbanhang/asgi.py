82de0dd77f94263fb4182ce23ad25d3f0b2573df
duc <ducnguyen042005@gmail.com>
2025-11-12 11:25:58 +0700

    Add base template and home view for the web application
    - Created a base HTML template for consistent layout across pages.
    - Added a home.html template that extends the base template.
    - Implemented a home view in views.py to render the home page.
    - Configured URLs to include the home view.
    - Updated settings and WSGI configurations for consistency.
    - Adjusted formatting in settings.py for better readability.

--- webbanhang/webbanhang/asgi.py
+++ webbanhang/webbanhang/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webbanhang.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webbanhang.settings")
 
 application = get_asgi_application()
--- webbanhang/webbanhang/wsgi.py
+++ webbanhang/webbanhang/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webbanhang.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webbanhang.settings")
 
 application = get_wsgi_application()
