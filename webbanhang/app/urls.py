cac65a13115b10a358a67638f89f7a481144ba27
longnm <you@example.com>
2025-11-13 22:06:02 +0700

    Completed #6: add cart.html, checkout.html

--- webbanhang/app/urls.py
+++ webbanhang/app/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-  
+    path('', views.home, name="home"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
 
 ]
