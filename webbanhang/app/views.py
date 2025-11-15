418609c1580df8605515f3f386fb6e4fd2ae0c79
duc <ducnguyen042005@gmail.com>
2025-11-15 21:44:24 +0700

    het video 9

--- webbanhang/app/views.py
+++ webbanhang/app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 # Create your views here.
 def home(request):
-    context= {}
-    return render(request,'app/home.html', context)
+    products = Product.objects.all()
+    
+    context= {'products': products}
+    return render(request,'app/home.html', context) 
 def cart(request):
     context= {}
     return render(request, 'app/cart.html', context)
