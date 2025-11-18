c4170e1b9f46326fbfb86bab31adab0e46287820
duc <ducnguyen042005@gmail.com>
2025-11-18 23:01:42 +0700

    video 13 xongn

---
+++ webbanhang/app/static/app/js/cart.js
@@ -0,0 +1,24 @@
+var updateBtns=document.getElementsByClassName('update-cart');
+
+for(i=0;i<updateBtns.length;i++){
+    updateBtns[i].addEventListener('click',function(){
+        var productId=this.dataset.product
+        var action=this.dataset.action
+        console.log('productId',productId,'action',action)
+        console.log('user: ',user)
+        if (user==="AnonymousUser"){
+            console.log('user not logged in')
+
+        }else{
+            console.log('user logged in, success add')
+        }
+    }
+    )
+}
+
+
+
+
+
+
+
