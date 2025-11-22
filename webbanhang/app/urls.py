404eaab2482d5be6334e51dcc8f0e4d0a6bfc525
longnm <you@example.com>
2025-11-22 19:47:14 +0700

    het video 20

--- webbanhang/app/urls.py
+++ webbanhang/app/urls.py
@@ -6,7 +6,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('register/', views.register, name="register"),
-    path('login/', views.login, name="login"),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutPage, name="logout"),
+    path('search/', views.search, name="search"),
     path('cart/', views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout"),
     path('update_item/', views.updateItem, name="update_item"),
